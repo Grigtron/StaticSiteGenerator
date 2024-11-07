b04a1d11f1cf1d72206d749ff32f44183e3d86bd
Grigtron <jgregas@gmail.com>
2024-11-06 21:02:13 -0500

    Added block to block type and unit testing

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from textnode import *
 from htmlnode import *
 
@@ -11,3 +12,23 @@ def markdown_to_blocks(markdown):
         if stripped_block:
             filtered_blocks.append(stripped_block)
     return filtered_blocks
+
+def block_to_block_type(text):
+    if text.startswith("#"):
+        hash_count = 0
+        for char in text:
+            if char == '#':
+                hash_count += 1
+            else:
+                break
+        if 1 <= hash_count <= 6 and text[hash_count] == ' ':
+            return 'heading'
+    elif text.startswith('```') and text.endswith('```'):
+        return 'code'
+    elif all(line.startswith('>') for line in text.split('\n')):
+        return 'quote'
+    elif all((line.startswith('*') and line[1] == ' ') or (line.startswith('-') and line[1] == ' ') for line in text.split('\n')):
+        return 'unordered_list'
+    elif all(line[0].isdigit() and int(line[0]) == (i + 1) and line[1] == '.' and line[2] == ' ' for i, line in enumerate(text.split('\n'))):
+        return 'ordered_list'
+
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -21,5 +21,35 @@ class TestBlockMarkdown(unittest.TestCase):
         ]
         self.assertEqual(result, expected_result)
 
+    def test_block_to_block_type_heading(self):
+        text = "#### This is a heading"
+        result = block_to_block_type(text)
+        expected_result = 'heading'
+        self.assertEqual(expected_result, result)
+
+    def test_block_to_block_type_code(self):
+        text = "```This is a code block```"
+        result = block_to_block_type(text)
+        expected_result = 'code'
+        self.assertEqual(expected_result, result)
+
+    def test_block_to_block_type_quote(self):
+        text = '>This is\n>a quote\n>test'
+        result = block_to_block_type(text)
+        expected_result = 'quote'
+        self.assertEqual(expected_result, result)
+    
+    def test_block_to_block_type_unordered_list(self):
+        text = '* This is a\n- unordered list test'
+        result = block_to_block_type(text)
+        expected_result = 'unordered_list'
+        self.assertEqual(expected_result, result)
+
+    def test_block_to_block_type_ordered_list(self):
+        text = '1. This is a\n2. ordered list\n3. test'
+        result = block_to_block_type(text)
+        expected_result = 'ordered_list'
+        self.assertEqual(expected_result, result)
+
 if __name__ == "__main__":
     unittest.main()
