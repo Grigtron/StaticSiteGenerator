05277034abd782d641e8e1fc6eac65c9e67e662c
Grigtron <jgregas@gmail.com>
2024-11-09 19:53:49 -0500

    Added recursive file moving between directories

--- src/main.py
+++ src/main.py
@@ -1,8 +1,34 @@
 # hello world
+import os
+import shutil
 from textnode import *
+from htmlnode import *
+from inline_markdown import *
 
 def main():
-    node =  TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    static_to_public_move("/home/grigaroni/workspace/github.com/Grigtron/static_site_generator/static", 
+                      "/home/grigaroni/workspace/github.com/Grigtron/static_site_generator/public")
+
+
+def static_to_public_move(source, destination):
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    else:
+        shutil.rmtree(destination)
+        os.mkdir(destination)
+    
+    entries = os.listdir(path=source)
+    for entry in entries:
+        entry_path = os.path.join(source, entry)
+        dest_path = os.path.join(destination, entry)
+
+        if os.path.isfile(entry_path):
+            shutil.copy(entry_path, dest_path)
+        elif os.path.isdir(entry_path):
+            os.mkdir(dest_path)
+            static_to_public_move(entry_path, dest_path)
+
+
+
 if __name__ == "__main__":
     main()
